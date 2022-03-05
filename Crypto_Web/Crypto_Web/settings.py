19b3fa1c893a0e68cb2eb2d52cf9b832a94acd6e
michaellai937 <jiachong.lai@mail.utoronto.ca>
2022-03-05 20:27:54 +0800

    remove secret key

--- Crypto_Web/Crypto_Web/settings.py
+++ Crypto_Web/Crypto_Web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+qwmmb737o)0!o_nihw*%qc-5kz&m653%(y-*@$@*=uwvn7&!n'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

d3fc5812f426365ee53aa41f6e67f6f2cbbc4b88
michaellai93708 <100908727+michaellai93708@users.noreply.github.com>
2022-03-10 10:36:08 +0800

    uploaded adminlte & necessary changes

--- Crypto_Web/Crypto_Web/views.py
+++ Crypto_Web/Crypto_Web/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
     return HttpResponse("Hello world! This is where everything starts!!!\n M.Lai 2022/03/05")
 
 def homepage(request):
-    return HttpResponse("This is supposed to be the homepage of our web!!!")    
+    context = {}
+    context['Homepage'] = 'Hello World!' 
+    return render(request, 'homepage.html', context)

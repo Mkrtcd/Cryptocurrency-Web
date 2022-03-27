10a7711a77d7ee4058957090ebad2f0c1728fd0d
michaellai93708 <100908727+michaellai93708@users.noreply.github.com>
2022-03-27 14:49:05 +0800

    commit

--- Crypto_Web/Crypto_Web/settings.py
+++ Crypto_Web/Crypto_Web/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'Crypto_Web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'user_info',
-        'USER': 'postgres',
-        'PASSWORD': '19960926',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+#DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql',
+    ##    'NAME': 'user_info',
+    #    'USER': 'postgres',
+    #    'PASSWORD': '19960926',
+    #    'HOST': 'localhost',
+    #    'PORT': '5432'
+    #}
+#}
 
 
 # Password validation

9c53c39acf260fb511d121ffbc01bbc7f5c7d6ca
michaellai93708 <100908727+michaellai93708@users.noreply.github.com>
2022-04-02 20:21:34 +0800

    created a sql.py  to check ability to connectds

---
+++ sql.py
@@ -0,0 +1,10 @@
+import psycopg2
+
+conn = psycopg2.connect(database="user_info", user="postgres", password="19960926", host="localhost")
+
+cur = conn.cursor()
+cur.execute("select * from users")
+rows = cur.fetchall()
+print(rows)
+conn.commit()
+conn.close()
